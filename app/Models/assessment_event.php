89d35626610ad8a3097e6ad484ad30379024a662
Md Sohag Hosen <sohag1426@gmail.com>
2026-02-25 12:04:54 +0600

    assessment event feedback % added

--- app/Models/assessment_event.php
+++ app/Models/assessment_event.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -54,5 +55,26 @@ class assessment_event extends Model
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id', 'id')->withDefault();
+    }
+
+    public function feedbackPercentage(): Attribute
+    {
+        return Attribute::make(
+            get: function ($value, $attributes) {
+
+                $studentCount = assessment_event_student::where('event_id', $attributes['id'])->count();
+                $done = assessment_status::where('event_id', $attributes['id'])->count();
+
+                if ($studentCount == 0) {
+                    return 0;
+                }
+
+                if ($done == 0) {
+                    return 0;
+                }
+
+                return round((($done / $studentCount) * 100), 2);
+            },
+        );
     }
 }
--- resources/views/teacher/assessment_events.blade.php
+++ resources/views/teacher/assessment_events.blade.php
@@ -1,209 +1,213 @@
 @extends ('laraview.layouts.sideNavLayout')
 
 @section('title')
-Feedback Events
+    Feedback Events
 @endsection
 
 @section('pageCss')
 @endsection
 
 @section('activeLink')
-@php
-$active_menu = '5';
-$active_link = '1';
-@endphp
+    @php
+        $active_menu = '5';
+        $active_link = '1';
+    @endphp
 @endsection
 
 @section('sidebar')
-@include('teacher.sidebar')
+    @include('teacher.sidebar')
 @endsection
 
 @section('contentTitle')
-<ul class="nav flex-column flex-sm-row ml-4">
-    <!--New New Feedback Event-->
-    <li class="nav-item">
-        <a class="btn btn-outline-success my-2 my-sm-0" href="{{ route('assessment_events.create') }}">
-            <i class="fas fa-plus"></i>
-            New Feedback Event
-        </a>
-    </li>
-    <!--/New New Feedback Event-->
-</ul>
+    <ul class="nav flex-column flex-sm-row ml-4">
+        <!--New New Feedback Event-->
+        <li class="nav-item">
+            <a class="btn btn-outline-success my-2 my-sm-0" href="{{ route('assessment_events.create') }}">
+                <i class="fas fa-plus"></i>
+                New Feedback Event
+            </a>
+        </li>
+        <!--/New New Feedback Event-->
+    </ul>
 @endsection
 
 @section('content')
-<div class="card">
+    <div class="card">
 
-    <!--modal -->
-    <div class="modal" tabindex="-1" role="dialog" id="modal-default">
-        <div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 id="modal-title" class="modal-title"></h4>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
+        <!--modal -->
+        <div class="modal" tabindex="-1" role="dialog" id="modal-default">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 id="modal-title" class="modal-title"></h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body overflow-auto" id="ModalBody">
+                    </div>
+                    <div class="modal-footer justify-content-between">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+                <!-- /modal-content -->
+            </div>
+            <!-- /modal-dialog -->
+        </div>
+        <!-- /modal -->
+
+        <div class="card-body">
+
+            {{-- @Filter --}}
+            <form class="d-flex align-content-start flex-wrap" action="{{ route('assessment_events.index') }}"
+                method="get">
+
+                {{-- user_id --}}
+                <div class="form-group col-md-4">
+                    <select name="user_id" id="user_id" class="form-control">
+                        <option value=''>Teacher...</option>
+                        @foreach ($users->sortBy('name') as $user)
+                            <option value="{{ $user->id }}">
+                                {{ $user->name }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+                {{-- user_id --}}
+
+                {{-- course_id --}}
+                <div class="form-group col-md-4">
+                    <select name="course_id" id="course_id" class="form-control">
+                        <option value=''>Course...</option>
+                        @foreach ($courses->sortBy('code') as $course)
+                            <option value="{{ $course->id }}">
+                                {{ $course->code }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+                {{-- course_id --}}
+
+                <div class="form-group col-md-2">
+                    <button type="submit" class="btn btn-dark">
+                        FILTER
                     </button>
                 </div>
-                <div class="modal-body overflow-auto" id="ModalBody">
-                </div>
-                <div class="modal-footer justify-content-between">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                </div>
-            </div>
-            <!-- /modal-content -->
+
+            </form>
+            {{-- @endFilter --}}
+
+            <table id="data_table" class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Teacher</th>
+                        <th scope="col">Course</th>
+                        <th scope="col">Student Group</th>
+                        <th scope="col">Start Time</th>
+                        <th scope="col">Stop Time</th>
+                        <th scope="col">Created By</th>
+                        <th scope="col">Score</th>
+                        <th scope="col">Feddback %</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($assessment_events as $assessment_event)
+                        <tr>
+                            <td scope="row">{{ $assessment_event->id }}</td>
+                            <td>{{ $assessment_event->teacher->name }}</td>
+                            <td>
+                                {{ $assessment_event->course->name }} ({{ $assessment_event->course->code }})
+                            </td>
+                            <td>{{ $assessment_event->group->name }}</td>
+                            <td>{{ $assessment_event->start_time }}</td>
+                            <td>{{ $assessment_event->stop_time }}</td>
+                            <td>{{ $assessment_event->user->name }}</td>
+
+                            @if (auth()->user()->can('viewScore', $assessment_event))
+                                <td>{{ $assessment_event->score }}</td>
+                            @else
+                                <td>***</td>
+                            @endif
+
+                            <td>{{ $assessment_event->feedback_percentage }} %</td>
+
+                            <td>
+
+                                <a class="btn btn-outline-info btn-sm mb-2"
+                                    href="{{ route('assessment_events.assessment_event_students.index', ['assessment_event' => $assessment_event]) }}">
+                                    <i class="fas fa-info-circle"></i>
+                                    Students
+                                </a>
+
+                                <a class="btn btn-outline-info btn-sm mb-2"
+                                    href="{{ route('assessment_events.status.index', ['assessment_event' => $assessment_event]) }}">
+                                    <i class="fas fa-info-circle"></i>
+                                    Feedback Status
+                                </a>
+
+                                {{-- Generate Report --}}
+                                @can('generateReport', $assessment_event)
+                                    <a class="btn btn-outline-info btn-sm mb-2"
+                                        href="{{ route('generate-score', ['assessment_event' => $assessment_event]) }}">
+                                        <i class="fas fa-user-edit"></i>
+                                        Generate Report
+                                    </a>
+                                @endcan
+                                {{-- Generate Report --}}
+
+                                {{-- Download Report --}}
+                                @can('downloadReport', $assessment_event)
+                                    <a class="btn btn-outline-info btn-sm mb-2"
+                                        href="{{ route('download-score', ['assessment_event' => $assessment_event]) }}">
+                                        <i class="fas fa-download"></i>
+                                        Download Report
+                                    </a>
+                                @endcan
+                                {{-- Download Report --}}
+
+                                {{-- Extend Time --}}
+                                @can('update', $assessment_event)
+                                    <a class="btn btn-outline-info btn-sm mb-2"
+                                        href="{{ route('assessment_events.extend_time.create', ['assessment_event' => $assessment_event]) }}">
+                                        <i class="fas fa-external-link-square-alt"></i>
+                                        Extend Time
+                                    </a>
+                                @endcan
+                                {{-- Extend Time --}}
+
+                                {{-- EDIT & DELETE --}}
+                                @can('delete', $assessment_event)
+                                    <a class="btn btn-outline-info btn-sm mb-2"
+                                        href="{{ route('assessment_events.edit', ['assessment_event' => $assessment_event]) }}">
+                                        <i class="fas fa-edit"></i>
+                                        EDIT
+                                    </a>
+                                    <form method="POST"
+                                        action="{{ route('assessment_events.destroy', ['assessment_event' => $assessment_event]) }}"
+                                        onsubmit="return confirm('Are you sure you want to remove the item?')">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-outline-danger btn-sm">DELETE</button>
+                                    </form>
+                                @endcan
+                                {{-- EDIT & DELETE --}}
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
         </div>
-        <!-- /modal-dialog -->
-    </div>
-    <!-- /modal -->
-
-    <div class="card-body">
-
-        {{-- @Filter --}}
-        <form class="d-flex align-content-start flex-wrap" action="{{ route('assessment_events.index') }}" method="get">
-
-            {{-- user_id --}}
-            <div class="form-group col-md-4">
-                <select name="user_id" id="user_id" class="form-control">
-                    <option value=''>Teacher...</option>
-                    @foreach ($users->sortBy('name') as $user)
-                    <option value="{{ $user->id }}">
-                        {{ $user->name }}
-                    </option>
-                    @endforeach
-                </select>
-            </div>
-            {{-- user_id --}}
-
-            {{-- course_id --}}
-            <div class="form-group col-md-4">
-                <select name="course_id" id="course_id" class="form-control">
-                    <option value=''>Course...</option>
-                    @foreach ($courses->sortBy('code') as $course)
-                    <option value="{{ $course->id }}">
-                        {{ $course->code }}
-                    </option>
-                    @endforeach
-                </select>
-            </div>
-            {{-- course_id --}}
-
-            <div class="form-group col-md-2">
-                <button type="submit" class="btn btn-dark">
-                    FILTER
-                </button>
-            </div>
-
-        </form>
-        {{-- @endFilter --}}
-
-        <table id="data_table" class="table table-hover">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Teacher</th>
-                    <th scope="col">Course</th>
-                    <th scope="col">Student Group</th>
-                    <th scope="col">Start Time</th>
-                    <th scope="col">Stop Time</th>
-                    <th scope="col">Created By</th>
-                    <th scope="col">Score</th>
-                    <th scope="col"></th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($assessment_events as $assessment_event)
-                <tr>
-                    <td scope="row">{{ $assessment_event->id }}</td>
-                    <td>{{ $assessment_event->teacher->name }}</td>
-                    <td>
-                        {{ $assessment_event->course->name }} ({{ $assessment_event->course->code }})
-                    </td>
-                    <td>{{ $assessment_event->group->name }}</td>
-                    <td>{{ $assessment_event->start_time }}</td>
-                    <td>{{ $assessment_event->stop_time }}</td>
-                    <td>{{ $assessment_event->user->name }}</td>
-
-                    @if (auth()->user()->can('viewScore', $assessment_event))
-                    <td>{{ $assessment_event->score }}</td>
-                    @else
-                    <td>***</td>
-                    @endif
-
-                    <td>
-
-                        <a class="btn btn-outline-info btn-sm mb-2"
-                            href="{{ route('assessment_events.assessment_event_students.index', ['assessment_event' => $assessment_event]) }}">
-                            <i class="fas fa-info-circle"></i>
-                            Students
-                        </a>
-
-                        <a class="btn btn-outline-info btn-sm mb-2"
-                            href="{{ route('assessment_events.status.index', ['assessment_event' => $assessment_event]) }}">
-                            <i class="fas fa-info-circle"></i>
-                            Feedback Status
-                        </a>
-
-                        {{-- Generate Report --}}
-                        @can('generateReport', $assessment_event)
-                        <a class="btn btn-outline-info btn-sm mb-2"
-                            href="{{ route('generate-score', ['assessment_event' => $assessment_event]) }}">
-                            <i class="fas fa-user-edit"></i>
-                            Generate Report
-                        </a>
-                        @endcan
-                        {{-- Generate Report --}}
-
-                        {{-- Download Report --}}
-                        @can('downloadReport', $assessment_event)
-                        <a class="btn btn-outline-info btn-sm mb-2"
-                            href="{{ route('download-score', ['assessment_event' => $assessment_event]) }}">
-                            <i class="fas fa-download"></i>
-                            Download Report
-                        </a>
-                        @endcan
-                        {{-- Download Report --}}
-
-                        {{-- Extend Time --}}
-                        @can('update', $assessment_event)
-                        <a class="btn btn-outline-info btn-sm mb-2"
-                            href="{{ route('assessment_events.extend_time.create', ['assessment_event' => $assessment_event]) }}">
-                            <i class="fas fa-external-link-square-alt"></i>
-                            Extend Time
-                        </a>
-                        @endcan
-                        {{-- Extend Time --}}
-
-                        {{-- EDIT & DELETE --}}
-                        @can('delete', $assessment_event)
-                        <a class="btn btn-outline-info btn-sm mb-2"
-                            href="{{ route('assessment_events.edit', ['assessment_event' => $assessment_event]) }}">
-                            <i class="fas fa-edit"></i>
-                            EDIT
-                        </a>
-                        <form method="POST"
-                            action="{{ route('assessment_events.destroy', ['assessment_event' => $assessment_event]) }}"
-                            onsubmit="return confirm('Are you sure you want to remove the item?')">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-outline-danger btn-sm">DELETE</button>
-                        </form>
-                        @endcan
-                        {{-- EDIT & DELETE --}}
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+        <!--/card body-->
 
     </div>
-    <!--/card body-->
-
-</div>
 @endsection
 
 @section('pageJs')
-<script>
-    function showGroupMembers(url) {
+    <script>
+        function showGroupMembers(url) {
             $("#modal-title").html("Students");
             $("#ModalBody").html('<div class="overlay"><i class="fas fa-sync-alt fa-spin"></i></div>');
             $("#ModalBody").append('<div class="text-bold pt-2">Loading...</div>');
@@ -213,5 +217,5 @@ $active_link = '1';
                 $("#ModalBody").html(data);
             });
         }
-</script>
+    </script>
 @endsection
