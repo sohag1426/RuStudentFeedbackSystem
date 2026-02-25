88ed951ca9444a0084a465f92dbe7b2b156500c4
Md Sohag Hosen <sohag1426@gmail.com>
2026-02-25 10:18:55 +0600

    Sidebar updated

--- resources/views/teacher/sidebar.blade.php
+++ resources/views/teacher/sidebar.blade.php
@@ -14,41 +14,42 @@
 
                 @php
 
-                $menu = [
-                '0' => 0,
-                '1' => 0,
-                '2' => 0,
-                '3' => 0,
-                '4' => 0,
-                '5' => 0,
-                '6' => 0,
-                '7' => 0,
-                ];
+                    $menu = [
+                        '0' => 0,
+                        '1' => 0,
+                        '2' => 0,
+                        '3' => 0,
+                        '4' => 0,
+                        '5' => 0,
+                        '6' => 0,
+                        '7' => 0,
+                    ];
 
-                $link = [
-                '0' => ['0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0],
-                '1' => ['0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0],
-                '2' => ['0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0],
-                '3' => ['0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0],
-                '4' => ['0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0, '6' => 0],
-                '5' => ['0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0],
-                '6' => ['0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0],
-                '7' => ['0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0],
-                ];
+                    $link = [
+                        '0' => ['0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0],
+                        '1' => ['0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0],
+                        '2' => ['0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0],
+                        '3' => ['0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0],
+                        '4' => ['0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0, '6' => 0],
+                        '5' => ['0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0],
+                        '6' => ['0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0],
+                        '7' => ['0' => 0, '1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0],
+                    ];
 
-                if (isset($active_menu)) {
-                $menu[$active_menu] = 1;
-                }
+                    if (isset($active_menu)) {
+                        $menu[$active_menu] = 1;
+                    }
 
-                if (isset($active_link)) {
-                $link[$active_menu][$active_link] = 1;
-                }
+                    if (isset($active_link)) {
+                        $link[$active_menu][$active_link] = 1;
+                    }
 
                 @endphp
 
                 <!--Dashboard menu[0]-->
                 <li class="nav-item">
-                    <a href="{{ route('dashboard') }}" class="nav-link @if ($menu['0']) active @endif ">
+                    <a href="{{ route('dashboard') }}"
+                        class="nav-link @if ($menu['0']) active @endif ">
                         <i class="fas fa-palette"></i>
                         <p>Dashboard</p>
                     </a>
@@ -56,19 +57,19 @@
                 <!--/Dashboard-->
 
                 {{-- Teachers --}}
-
                 <li class="nav-item">
-                    <a href="{{ route('users.index') }}" class="nav-link @if ($menu['1']) active @endif">
+                    <a href="{{ route('users.index') }}"
+                        class="nav-link @if ($menu['1']) active @endif">
                         <i class="fas fa-users-cog"></i>
                         <p>Teachers</p>
                     </a>
                 </li>
-
                 {{-- Teachers --}}
 
                 {{-- Officers/Staffs --}}
                 <li class="nav-item">
-                    <a href="{{ route('department-manager.index') }}" class="nav-link @if ($menu['6']) active @endif">
+                    <a href="{{ route('department-manager.index') }}"
+                        class="nav-link @if ($menu['6']) active @endif">
                         <i class="fas fa-users-cog"></i>
                         <p>Officers/Staffs</p>
                     </a>
@@ -77,7 +78,8 @@
 
                 {{-- courses --}}
                 <li class="nav-item">
-                    <a href="{{ route('courses.index') }}" class="nav-link @if ($menu['2']) active @endif">
+                    <a href="{{ route('courses.index') }}"
+                        class="nav-link @if ($menu['2']) active @endif">
                         <i class="fas fa-book"></i>
                         <p>Courses</p>
                     </a>
@@ -86,7 +88,8 @@
 
                 {{-- student group --}}
                 <li class="nav-item">
-                    <a href="{{ route('student_groups.index') }}" class="nav-link @if ($menu['3']) active @endif">
+                    <a href="{{ route('student_groups.index') }}"
+                        class="nav-link @if ($menu['3']) active @endif">
                         <i class="fas fa-user-graduate"></i>
                         <p>Students</p>
                     </a>
@@ -95,29 +98,30 @@
 
                 {{-- questions --}}
                 @if (auth('web')->user()->role != 'DepartmentManager')
-                <li class="nav-item">
-                    <a href="{{ route('questions.index') }}" class="nav-link @if ($menu['4']) active @endif">
-                        <i class="fas fa-question"></i>
-                        <p>Questions For Feedback</p>
-                    </a>
-                </li>
+                    <li class="nav-item">
+                        <a href="{{ route('questions.index') }}"
+                            class="nav-link @if ($menu['4']) active @endif">
+                            <i class="fas fa-question"></i>
+                            <p>Questions For Feedback</p>
+                        </a>
+                    </li>
                 @endif
                 {{-- questions --}}
 
                 {{-- Feedback events --}}
-                @if (auth('web')->user()->role != 'DepartmentManager')
                 <li class="nav-item">
-                    <a href="{{ route('assessment_events.index') }}" class="nav-link @if ($menu['5']) active @endif">
+                    <a href="{{ route('assessment_events.index') }}"
+                        class="nav-link @if ($menu['5']) active @endif">
                         <i class="fas fa-calendar-alt"></i>
                         <p>Feedback Events</p>
                     </a>
                 </li>
-                @endif
                 {{-- Feedback events --}}
 
                 {{-- change Logs --}}
                 <li class="nav-item">
-                    <a href="{{ route('change-logs') }}" class="nav-link @if ($menu['7']) active @endif">
+                    <a href="{{ route('change-logs') }}"
+                        class="nav-link @if ($menu['7']) active @endif">
                         <i class="fas fa-user-shield"></i>
                         <p>Change Logs</p>
                     </a>
