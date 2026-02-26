3373785c207a91beaf5a46e6db86b939224cc00f
Md Sohag Hosen <sohag1426@gmail.com>
2026-02-26 11:13:23 +0600

    updated

--- app/Policies/StudentGroupMemberPolicy.php
+++ app/Policies/StudentGroupMemberPolicy.php
@@ -2,8 +2,8 @@
 
 namespace App\Policies;
 
-use App\Models\User;
 use App\Models\student_group_member;
+use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class StudentGroupMemberPolicy
@@ -13,7 +13,6 @@ class StudentGroupMemberPolicy
     /**
      * Determine whether the user can view any models.
      *
-     * @param  \App\Models\User  $user
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function viewAny(User $user)
@@ -24,8 +23,6 @@ class StudentGroupMemberPolicy
     /**
      * Determine whether the user can view the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\student_group_member  $studentGroupMember
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function view(User $user, student_group_member $studentGroupMember)
@@ -36,7 +33,6 @@ class StudentGroupMemberPolicy
     /**
      * Determine whether the user can create models.
      *
-     * @param  \App\Models\User  $user
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function create(User $user)
@@ -47,8 +43,6 @@ class StudentGroupMemberPolicy
     /**
      * Determine whether the user can update the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\student_group_member  $studentGroupMember
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function update(User $user, student_group_member $studentGroupMember)
@@ -59,23 +53,20 @@ class StudentGroupMemberPolicy
     /**
      * Determine whether the user can delete the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\student_group_member  $studentGroupMember
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function delete(User $user, student_group_member $studentGroupMember)
     {
-        if ($user->department_id == $studentGroupMember->department_id && $user->role == 'department_admin') {
+        if ($user->department_id == $studentGroupMember->department_id && $user->role == 'DepartmentManager') {
             return true;
         }
+
         return false;
     }
 
     /**
      * Determine whether the user can restore the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\student_group_member  $studentGroupMember
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function restore(User $user, student_group_member $studentGroupMember)
@@ -86,8 +77,6 @@ class StudentGroupMemberPolicy
     /**
      * Determine whether the user can permanently delete the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\student_group_member  $studentGroupMember
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function forceDelete(User $user, student_group_member $studentGroupMember)
