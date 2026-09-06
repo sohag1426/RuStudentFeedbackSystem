e129b01f8e1f4decd1cf8e7401d9513499928e70
Md. Sohag Hosen <sohag1426@gmail.com>
2026-09-06 10:52:47 +0600

    feat: refactor remove-student-from-feedback-event authorization logic for consistency

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -38,8 +38,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Gate::define('remove-student-from-feedback-event', function (User $user, assessment_event $assessment_event, assessment_event_student $assessment_event_student) {
-            if (assessment_status::where('event_id', $assessment_event->id)->where('student_id', $assessment_event_student->student_id)->count()) {
+        Gate::define('remove-student-from-feedback-event', function (User $user, AssessmentEvent $assessmentEvent, AssessmentEventStudent $assessmentEventStudent) {
+            if (AssessmentStatus::where('event_id', $assessmentEvent->id)->where('student_id', $assessmentEventStudent->student_id)->count()) {
                 return false;
             }
 
